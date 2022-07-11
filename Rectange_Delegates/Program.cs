59b09a8363db5bdbda915b2f9993d90a75116963
Akhil P Thomas <akhilthomas1498@gmail.com>
2022-07-11 12:39:59 +0530

    Add project files.

---
+++ Rectange_Delegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using static Rectange_Delegates.Rectange;
+
+namespace Rectange_Delegates
+{
+    public class Rectange
+    {
+        public delegate void RectDelegate(Double h, Double w);
+        public void area(Double h, Double w)
+        {
+            Console.Write("\nArea = {0}", w * h);
+        }
+        public void peri(Double h, Double w)
+        {
+            Console.Write("\nPerimeter = {0}", 2*(w + h));
+        }
+   
+        static void Main(string[] args)
+        {
+            Rectange rect = new Rectange();
+            RectDelegate rectdel = new RectDelegate(rect.area);
+            rectdel += rect.peri;
+            rectdel.Invoke(5,8);
+            Console.Read();
+        }
+    }
+}
